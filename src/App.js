dcea081805d16947bcb55aa96d82537a7b996bef
Raheem Fatai <raheemadebayorr@gmail.com>
2023-02-10 06:58:55 +0000

    Added a sign up page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-
+import {Routes, Route} from 'react-router-dom'
 import './App.css';
 
 function App() {
   return (
   <div className="App">
     <Routes>
-      <Route path="/" element = {<Home/>} />
-      <Route path="/" element = {<Home/>} />
-      <Route path="/" element = {<Home/>} />
+      <Route path="/" element = {<Home />} />
+      <Route path="sign_up" element = {<Home />} />
+      <Route path="/" element = {<Home/>} /> 
     </Routes>
   </div>
   );
