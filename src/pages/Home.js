fb28ecdea7beaba0320a5521f24fa2fd719bc8c3
Raheem Fatai <raheemadebayorr@gmail.com>
2023-02-10 09:29:15 +0000

    work on the header and hero component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,16 @@
 import React from "react"
+import Header from "../components/Header"
+import Hero from "../components/hero-section"
 
 const Home = () => {
     return (
-        <div>Home</div>
+        <>
+        <div>
+            <Header />
+            <Hero />
+        </div>
+        </>
     )
 }
+
 export default Home
