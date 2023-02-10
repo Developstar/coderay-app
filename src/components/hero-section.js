c113dfa1ef10990f8667aacce07ae4299bb7f038
Raheem Fatai <raheemadebayorr@gmail.com>
2023-02-10 09:42:07 +0000

    Edied Hero

--- src/components/hero-section.js
+++ src/components/hero-section.js
@@ -4,7 +4,7 @@ const Hero = () => {
     return (
         <div className='Hero-section'>
             <h1 className='hero-title'>Welcome To CodeRay Editor</h1>
-            <p>Are you looking for a efficient online code editor?</p>
+            <p className='hero-subtitle'>Are you looking for a efficient online code editor?</p>
             <p>Look no further! CodeRay offers a fast and intuitive coding experience so you can stay productive and creative</p>
             <p>With CodeRay, you can write clean JavaScript and CSS code</p>
             <a className='CTA'>Try CodeRay Now</a>
