cf5ca24661f38fcdedca2d253d1b01c412c34b15
Developstar <deyeye99@gmail.com>
2023-02-11 08:16:26 +0100

    appjs edited

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import {Routes, Route} from "react-router-dom"
-import Home from './pages/Home'
+import Home from "./Pages/Home" ;
+import SignUp from './Pages/SignUp';
 
 
 function App() {
@@ -8,6 +9,8 @@ function App() {
   <div className="App">
     <Routes>
       <Route path="/" element = {<Home/>} />
+      <Route path="/form" element = {<SignUp/>} />
+      
 
     </Routes>
   </div>
--- src/pages/sign_up.js
+++
@@ -1,7 +0,0 @@
-const signUp  = () => {
-    return (
-        <div>Sign Up Page</div>
-    )
-}
-
-export default signUp
